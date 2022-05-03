1ecaa1a4766f90600f3f04246e231e66008194cd
Het Patel <75133701+HET-CE@users.noreply.github.com>
2022-05-03 22:18:44 +0530

    Delete src/main/java/com/herokuapp/QuizforProgrammingLanguages/users directory

--- src/main/java/com/herokuapp/QuizforProgrammingLanguages/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.herokuapp.QuizforProgrammingLanguages.users;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<Users, String> {
-
-}
